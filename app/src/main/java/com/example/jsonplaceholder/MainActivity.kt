ab1143033cfe48b73ae7e5606672c3d9976ca363
Daniel Naranjo <mr.daniel.naranjo@protonmail.com>
2022-04-05 16:05:02 -0400

    making call to api

--- app/src/main/java/com/example/jsonplaceholder/MainActivity.kt
+++ app/src/main/java/com/example/jsonplaceholder/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.jsonplaceholder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jsonplaceholder.databinding.ActivityMainBinding
+import retrofit2.HttpException
+import java.io.IOException
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+
+        lifecycleScope.launchWhenCreated {
+            binding.progressBar.isVisible = true
+            val response = try {
+                RetrofitInstance.api.getTodos()
+            }catch(e: IOException){
+                Log.e(TAG, "IOException, you might not have internet connection")
+                return@launchWhenCreated
+            }catch(e: HttpException){
+                Log.e(TAG, "HttpException, unexpected response")
+                return@launchWhenCreated
+            }
+            if(response.isSuccessful && response.body() != null)
+
+        }
+
+
     }
+
+    private fun setupRecyclerView() = binding.rvTodos.apply {
+        todoAdapter = TodoAdapter()
+        adapter = todoAdapter
+        layoutManager  = LinearLayoutManager(this@MainActivity)
+    }
+
 }
